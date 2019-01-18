ed3aaeb8aaae6cb5779eb688cfb92f3e3c368b31
EnmanuelAlx <EnmanuelAlx@gmail.com>
2019-01-18 00:00:54 -0400

    Se agrego la vista de home con los productos y el boton con su respectiva funcionalidad de añadir al carrito

--- app/order.php
+++ app/order.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class order extends Model
 {
-    //
+    protected $table = 'orders';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'emp_id', 'status', 'total'
+    ];
+
+
 }
