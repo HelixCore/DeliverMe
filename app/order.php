dfdf4dbabfeca2585cafe07ffc95b1bfaf84de7e
Eduardo Ascanio <eduardo.ascanio28@gmail.com>
2019-01-18 00:14:27 -0400

    A

--- app/order.php
+++ app/order.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class order extends Model
 {
-    //
+    protected $fillable = ['user_id','emp_id','status'];
+
+    public function get(Type $var = null)
+{
+    
 }
+}
+
+$orden = order::where('user_id', '=', Auth::user()->id)->where('status', '=', '0')->get();
+
+
