efa7c2d864a5dff41613049993646a57b8143b2c
Eduardo Ascanio <eduardo.ascanio28@gmail.com>
2019-01-18 00:14:34 -0400

    A

--- app/order.php
+++ app/order.php
@@ -8,10 +8,11 @@ class order extends Model
 {
     protected $fillable = ['user_id','emp_id','status'];
 
-    public function get(Type $var = null)
+    public function getPrecio(Type $var = null)
 {
     
 }
+
 }
 
 $orden = order::where('user_id', '=', Auth::user()->id)->where('status', '=', '0')->get();
